62f282d5d010b734f4a2011d2af0db6bd894e106
Valentin Omnès <valentin.omnes@gmail.com>
2020-01-19 18:45:10 +0100

    feat: Add statistic graph

--- src/components/LaunchStatistics/LaunchStatistics.tsx
+++ src/components/LaunchStatistics/LaunchStatistics.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
 import './styles.scss';
 import StatInterface from './model';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 interface Props {
   data: StatInterface;
 }
 
+interface GraphData {
+  name: String,
+  uv: Number,
+}
+
+const createGraphData = (data: any) => {
+  let visualGraph: GraphData[] = [];
+  Object.keys(data).forEach(key => {;
+    visualGraph.push({
+      name: key,
+      uv: data[key],
+    });
+  });
+  return visualGraph;
+}
+
 const LaunchStatistics: React.FC<Props> = ({ data }) => {
-  console.log(data)
+  const visualGraph = createGraphData(data.perYear)
+  console.log(visualGraph);
   return (
     <div className="launchStatistics">
       <h1 className="text__title">Statistics</h1>
@@ -15,8 +33,14 @@ const LaunchStatistics: React.FC<Props> = ({ data }) => {
         Success : { data.total.success }<br/>
         Fail : { data.total.fail }<br/>
         Total : { data.total.success + data.total.fail }<br/>
-        Upcoming : { data.upcoming }<br/>
+        Upcoming launches : { data.upcoming }<br/>
       </p>
+      <LineChart width={500} height={250} data={visualGraph}>
+        <Line type="monotone" dataKey="uv" stroke="#3498db" />
+        <CartesianGrid stroke="#2c3e50" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </div>
   )
 };
